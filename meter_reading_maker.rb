5fa078c5ff667532826abe784ddb87a2553ef89b
Prima Aulia Gusta <prima@tablevibe.co>
2024-05-07 15:22:40 +0800

    initial attempt without test

--- meter_reading_maker.rb
+++ meter_reading_maker.rb
@@ -5,8 +5,16 @@ class MeterReaderMaker
   def initialize(filepath)
     @filepath = filepath
     @sql_queries = []
+    @current_nmi = ""
+    @consumption_data = {}
+    @sql_statements = []
 
     parse_csv
+
+  end
+
+  def call
+    create_statements
   end
 
   private
@@ -17,8 +25,8 @@ class MeterReaderMaker
 
       case record_indicator
       when 100 then handle_header
-      when 200 then puts "nmi"
-      when 300 then puts "interval data"
+      when 200 then handle_nmi_data(row)
+      when 300 then handle_interval(row) && @current_nmi
       when 400 then puts "interval event"
       when 500 then puts "b2b"
       when 900 then puts "end of data"
@@ -28,9 +36,34 @@ class MeterReaderMaker
     end
   end
 
+  def create_statements
+    @consumption_data.each do |nmi, intervals|
+      intervals.each do |interval_date, consumption_values|
+        total_consumption = consumption_values.sum
+        sql = "INSERT INTO meter_readings (nmi, timestamp, consumption) VALUES ('#{nmi}', '#{interval_date}', #{total_consumption});"
+        @sql_statements << sql
+      end
+    end
+
+    @sql_statements
+  end
+
+  def handle_nmi_data(row)
+    @current_nmi = row[1]
+  end
+
   def handle_header
     puts "this is a header"
   end
+
+  def handle_interval(row)
+    interval_date = row[1]
+    consumption_values = row[2..50].map(&:to_f).sum
+
+    @consumption_data[@current_nmi] ||= {}
+    @consumption_data[@current_nmi][interval_date] ||= []
+    @consumption_data[@current_nmi][interval_date] << consumption_values
+  end
 end
 
-MeterReaderMaker.new("./data.csv")
+p MeterReaderMaker.new("./data.csv").call
