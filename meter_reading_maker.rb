c2b951acbaeab412e54f54c67b82b19715cd18fa
Prima Aulia Gusta <prima@tablevibe.co>
2024-05-09 17:46:08 +0800

    refactor to minimize o(n) square

--- meter_reading_maker.rb
+++ meter_reading_maker.rb
@@ -7,9 +7,8 @@ class MeterReaderMaker
   def initialize(filepath)
     @filepath = filepath
     @current_nmi = ""
-    @sql_statements = []
+    @sql_statements = {}
     @consumption_unit = ""
-    @consumption_data = {}
     @file_validator = %w[900 100] # works like a stack. the file is valid if it starts with 100 record and ends with 900 record
 
     parse_file
@@ -30,7 +29,7 @@ class MeterReaderMaker
       process_interval_records(line) if can_process_line?(line)
     end
 
-    prepare_sql_statements
+    merge_sql_statements
   end
 
   def process_nmi_record(line)
@@ -43,31 +42,34 @@ class MeterReaderMaker
 
   def process_interval_records(line)
     parts = line.chomp.split(',')
-    interval_date = parts[1]
+    time = prepare_timestamp(parts[1])
     consumption_values = parts[2...last_interval_index].map(&:to_f)
 
-    @consumption_data[@current_nmi] ||= {}
-    @consumption_data[@current_nmi][interval_date] ||= {}
-    @consumption_data[@current_nmi][interval_date][:values] ||= []
-    @consumption_data[@current_nmi][interval_date][:values] << consumption_values
-    @consumption_data[@current_nmi][interval_date][:unit] = @consumption_unit
-  end
+    @sql_statements[@current_nmi] ||= []
 
-  def prepare_sql_statements
-    @consumption_data.each do |nmi, current_consumption|
-
-      current_consumption.each do |interval_date, data|
-        time = prepare_timestamp(interval_date)
-        summed_values = data[:values].transpose.map { |sub_array| sub_array.sum }
-        summed_values.each do |value|
-          timestamp = time.strftime("%Y-%m-%d %H:%M")
-          value = convert_to_kwh(value) if data[:unit] != 'kwh'
-          @sql_statements << "INSERT INTO meter_readings ('nmi', 'timestamp', 'consumption') VALUES ('#{nmi}', '#{timestamp}', #{value});"
-
-          time += @interval_length * 60
-        end
+    if @sql_statements[@current_nmi].empty?
+      @sql_statements[@current_nmi] = consumption_values.map do |value|
+        statement = "INSERT INTO meter_readings ('nmi', 'timestamp', 'consumption') VALUES ('#{@current_nmi}', '#{time}', #{value});"
+        time += @interval_length * 60
+        statement
+      end
+    else
+      @sql_statements[@current_nmi] = @sql_statements[@current_nmi].map.with_index do |statement, index|
+        pattern = /,\s(\d+\.?\d*)/
+        inserted_value = statement.match(pattern)[1].to_f
+        new_value = inserted_value + consumption_values[index]
+        statement = statement.gsub(pattern, ", #{new_value}")
+        statement
       end
     end
+  end
+
+  def merge_sql_statements
+    flattened_array = []
+    @sql_statements.each_value do |value|
+      flattened_array.concat(value.flatten)
+    end
+    @sql_statements = flattened_array
   end
 
   def validate(line)
