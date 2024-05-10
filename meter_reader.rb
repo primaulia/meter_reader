45bee6d04155d4cff36dee3b37125f0c6119d778
Prima Aulia Gusta <prima@tablevibe.co>
2024-05-10 13:23:02 +0800

    refactor file names and unused variables

--- meter_reader.rb
+++ meter_reader.rb
@@ -1,14 +1,13 @@
 require 'csv'
 require 'pry-byebug'
 
-class MeterReaderMaker
-  NMI_PATTERN = /^200,(?<nmi>\w+),\w+,(\w+)?,(?<nmi_suffix>\w+),(\w+)?,\w+,(?<consumption_unit>\w+),(?<interval_length>\d+),(\w+)?$/
+class MeterReader
+  NMI_PATTERN = /^200,(?<nmi>\w+),\w+,(\w+)?,(?<nmi_suffix>\w+),(\w+)?,\w+,(?<current_consumption_unit>\w+),(?<interval_length>\d+),(\w+)?$/
 
   def initialize(filepath)
     @filepath = filepath
-    @current_nmi = ""
+    @current_meter = {}
     @sql_statements_by_nmi = {}
-    @consumption_unit = ""
     @file_validator = %w[900 100] # works like a stack. the file is valid if it starts with 100 record and ends with 900 record
 
     parse_file
@@ -32,10 +31,12 @@ class MeterReaderMaker
 
   def process_nmi_record(line)
     match_groups = line.match(NMI_PATTERN)
-    @current_nmi = match_groups[:nmi]
-    @consumption_unit = match_groups[:consumption_unit].downcase
-    @interval_length = match_groups[:interval_length].to_i # 5, 15, 30 minutes
-    @suffix = match_groups[:nmi_suffix].downcase
+    @current_meter = {
+      nmi: match_groups[:nmi],
+      unit: match_groups[:current_consumption_unit].downcase,
+      interval_length: match_groups[:interval_length].to_i,
+      nmi_suffix: match_groups[:nmi_suffix].downcase
+    }
   end
 
   def process_interval_records(line)
@@ -43,28 +44,28 @@ class MeterReaderMaker
     time = prepare_timestamp(parts[1])
     consumption_values = parts[2...last_interval_index].map(&:to_f)
 
-    @sql_statements_by_nmi[@current_nmi] ||= []
+    @sql_statements_by_nmi[current_nmi] ||= []
 
-    if @sql_statements_by_nmi[@current_nmi].empty?
-      @sql_statements_by_nmi[@current_nmi] = transform_values_to_statements(consumption_values, time)
+    if @sql_statements_by_nmi[current_nmi].empty?
+      @sql_statements_by_nmi[current_nmi] = transform_values_to_statements(consumption_values, time)
     else
-      @sql_statements_by_nmi[@current_nmi] = merge_values_to_existing_statements(consumption_values)
+      @sql_statements_by_nmi[current_nmi] = merge_values_to_existing_statements(consumption_values)
     end
   end
 
   def transform_values_to_statements(consumption_values, time)
     consumption_values.map do |value|
-      statement = "INSERT INTO meter_readings ('nmi', 'timestamp', 'consumption') VALUES ('#{@current_nmi}', '#{time}', #{value});"
-      time += @interval_length * 60
+      statement = "INSERT INTO meter_readings ('nmi', 'timestamp', 'consumption') VALUES ('#{current_nmi}', '#{time}', #{value});"
+      time += current_interval_length * 60
       statement
     end
   end
 
   def merge_values_to_existing_statements(consumption_values)
-    @sql_statements_by_nmi[@current_nmi].map.with_index do |statement, index|
+    @sql_statements_by_nmi[current_nmi].map.with_index do |statement, index|
       pattern = /,\s(\d+\.?\d*)/
       inserted_value = statement.match(pattern)[1].to_f
-      new_value = inserted_value + consumption_values[index]
+      new_value = inserted_value + consumption_values[index] # assumed that each repeated NMI data will have the same interval length
       statement = statement.gsub(pattern, ", #{new_value}")
       statement
     end
@@ -83,32 +84,48 @@ class MeterReaderMaker
     @file_validator.empty?
   end
 
+  def current_nmi
+    @current_meter[:nmi]
+  end
+
+  def current_consumption_unit
+    @current_meter[:unit]
+  end
+
+  def current_suffix
+    @current_meter[:nmi_suffix]
+  end
+
+  def current_interval_length
+    @current_meter[:interval_length]
+  end
+
   # only process line if it passes this condition
   # - is a 300 record
-  # - have @current_nmi state, set up
+  # - have current_nmi state, set up
   # - the unit we worked with is in Watt Hour format (active consumption)
   # - export data only (nmi suffix E1 or E2)
   def can_process_interval_record?(line)
-    line.start_with?('300') && @current_nmi && @consumption_unit.include?('wh') && @suffix.include?("e")
+    line.start_with?('300') && current_nmi && current_consumption_unit.include?('wh') && current_suffix.include?("e")
   end
 
   def last_interval_index
-    (1440 / @interval_length) + 2
+    (1440 / current_interval_length) + 2
   end
 
   def convert_to_kwh(value)
-    case @consumption_unit
-    when 'MWh' then value * 1000
-    when 'Wh' then value.to_f / 1000
+    case current_consumption_unit
+    when 'mwh' then value * 1000
+    when 'wh' then value.to_f / 1000
     end
   end
 
   def prepare_timestamp(interval_date)
     date = Date.parse(interval_date)
-    Time.new(date.year, date.month, date.day, 0, @interval_length, 0) # the initial timestamp will be at the end of the interval length
+    Time.new(date.year, date.month, date.day, 0, current_interval_length, 0) # the initial timestamp will be at the end of the interval length
   end
 end
 
-statements = MeterReaderMaker.new("./fixtures/sample1.csv").call
+statements = MeterReader.new("./fixtures/sample1.csv").call
 binding.pry
 p statements
